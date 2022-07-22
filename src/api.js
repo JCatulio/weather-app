3a4578bc19e6664ac924f5b2909fc97bb261e345
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-07-22 15:55:01 -0300

    weatherApp

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1bf0f675camshef6d844037cea2bp171823jsn6d823a2d0168",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f2f2878e6382e1eb2420d73d63a63dde";
